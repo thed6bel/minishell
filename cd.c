a3818d0254d5f5cf43889112b6d71a5eef71010d
thed6bel <40074132+thed6bel@users.noreply.github.com>
2023-05-02 14:45:22 +0200

    Update cd.c

--- cd.c
+++ cd.c
@@ -12,6 +12,11 @@
 
 #include "../../minishell.h"
 
+/*
+ne pas oublier une fonction qui copie le double tab de **env vers liste chain2e
+et l'inverse pour envoyer un double tableau vers execve
+*/
+
 void    ft_upd_pwd(t_env *envp)
 {
     t_env *old;
@@ -21,22 +26,27 @@ void    ft_upd_pwd(t_env *envp)
 
     old = envp;
     new = envp;
-    i = -1;
-    while (old != NULL && ft_strcmp(old->var, "OLDPWD") == 0)
+    i = 0;
+    while (old != NULL && ft_strncmp(old->var, "OLDPWD", 6) != 0)
         old = old->next;
-    while (new != NULL && ft_strcmp(new->var, "PWD") == 0)
+    while (new != NULL && ft_strncmp(new->var, "PWD", 3) != 0)
         new = new->next;
     free(old->value[0]);
     ret = malloc(sizeof(char) * (ft_strlen(new->value[0]) + 1));
-    if (ret == NULL)
-		return (NULL);
-    while (new->value[0][i++])
+    if (!ret)
+		return ;
+    while (new->value[0][i])
+	{
 		ret[i] = new->value[0][i];
+		i++;
+	}
 	ret[i] = '\0';
     old->value[0] = ret;
     free(new->value[0]);
     new->value[0] =  getcwd(NULL, 0);
 }
+
+
 
 void    ft_builtin_cd(t_data *cmd, t_env *envp)
 {
@@ -52,3 +62,5 @@ void    ft_builtin_cd(t_data *cmd, t_env *envp)
     }
     //ont met un pointeur sur envp pour recupere la nouvelle valeur
 }
+
+//test semble ok a ctrl quand meme!!!
