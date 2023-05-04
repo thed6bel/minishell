8b781ade6ac44456983f9679597c6292839142b8
thed6bel <40074132+thed6bel@users.noreply.github.com>
2023-05-04 13:46:28 +0200

    changement de **value à *value

--- envp.c
+++ envp.c
@@ -17,56 +17,47 @@
 typedef struct s_env {
     char *var;
     char equal;
-    char **value;
+    char *value;
     struct s_env *next;
 } t_env;
 
 t_env *get_env_list(char **envp)
 {
-	int i;
-	char *equal_pos;
-	int var_len;
-	t_env *new_entry;
-	t_env *head;
-	t_env *tail;
-	char **value;
+    int i;
+    char *equal_pos;
+    int var_len;
+    t_env *new_entry;
+    t_env *head;
+    t_env *tail;
 
-	head = NULL;
-	tail = NULL;
-	if (envp == NULL || *envp == NULL)
-		return NULL;
-	i = 0;
-	while (envp[i] != NULL)
-	{
-		equal_pos = ft_strchr(envp[i], '=');
-		if (equal_pos != NULL)
-		{
-			new_entry = (t_env *)malloc(sizeof(t_env));
-			var_len = equal_pos - envp[i];
-			new_entry->var = ft_substr(envp[i], 0, var_len);
-			new_entry->equal = '=';
-			value = ft_split(equal_pos + 1, ':');
-			if (value == NULL)
-			{
-				free(new_entry->var);
-				free(new_entry);
-				i++;
-				continue;
-			}
-			new_entry->value = value;
-			new_entry->next = NULL;
-			if (tail == NULL)
-			{
-				head = new_entry;
-				tail = new_entry;
-			}
-			else
-			{
-				tail->next = new_entry;
-				tail = new_entry;
-			}
-		}
-		i++;
-	}
-	return head;
+    head = NULL;
+    tail = NULL;
+    if (envp == NULL || *envp == NULL)
+        return NULL;
+    i = 0;
+    while (envp[i] != NULL)
+    {
+        equal_pos = ft_strchr(envp[i], '=');
+        if (equal_pos != NULL)
+        {
+            new_entry = (t_env *)malloc(sizeof(t_env));
+            var_len = equal_pos - envp[i];
+            new_entry->var = ft_substr(envp[i], 0, var_len);
+            new_entry->equal = '=';
+            new_entry->value = ft_strdup(equal_pos + 1);
+            new_entry->next = NULL;
+            if (tail == NULL)
+            {
+                head = new_entry;
+                tail = new_entry;
+            }
+            else
+            {
+                tail->next = new_entry;
+                tail = new_entry;
+            }
+        }
+        i++;
+    }
+    return head;
 }
