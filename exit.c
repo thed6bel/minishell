6e04de3ea73f8d0a5b75328b0f5e2c744902a13a
thed6bel <40074132+thed6bel@users.noreply.github.com>
2023-05-09 08:25:12 +0200

    Add files via upload

--- exit.c
+++ exit.c
@@ -3,12 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hucorrei <hucorrei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: thed6bel <thed6bel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/28 13:49:25 by hucorrei          #+#    #+#             */
-/*   Updated: 2023/04/28 13:49:29 by hucorrei         ###   ########.fr       */
+/*   Created: 2023/05/08 20:22:55 by thed6bel          #+#    #+#             */
+/*   Updated: 2023/05/08 20:38:50 by thed6bel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../minishell.h"
+#include "../../minishell.h"
 
+int g_status;
+
+void	built_exit(t_data *cmd, t_env *envp)
+{
+    int i;
+    int mod;
+
+    i = 0;
+    mod = 0;
+    if (cmd->cmd[1] != NULL)
+    {
+        while (cmd->cmd[1][i])
+            if (!ft_isdigit(cmd->cmd[1][i++]))
+                //ft_error("bad argument");
+        if (cmd->cmd[1][i] == '\0')
+        {
+            g_status = ft_atoi(cmd->cmd[1]);
+            mod = 1;
+        }
+    }
+    if (cmd->cmd[1] != NULL && cmd->cmd[2] != NULL)
+        //ft_error("too many argument");
+    ft_free_env(envp);
+    if (mod == 0)
+        g_status = 0;
+    exit(g_status);
+        
+}
