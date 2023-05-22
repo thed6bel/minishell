def1c91f538fa608492a8014cf5ea1eacf94eeee
thed6bel <40074132+thed6bel@users.noreply.github.com>
2023-05-22 20:21:10 +0200

    Update exec.c

--- exec.c
+++ exec.c
@@ -38,46 +38,46 @@ void	ft_free(char **str)
 
 void ft_execute_commandes(t_prompt *p)
 {
-    int fd[2];
-    pid_t pid;
-    t_prompt *p1;
-    t_mini *n1;
-    int in;
+	int			fd[2];
+	pid_t		pid;
+	t_prompt	*p1;
+	t_mini		*n1;
+	int 		in;
 
-    in = 0;
-    p1 = p;
-    n1 = p->cmds->content;
-    while (p1->cmds != NULL)
-    {
-        if (pipe(fd) == -1)
-            ft_exit("pipe error:");
-        pid = fork();
-        if (pid == -1)
-            ft_exit("fork error:");
-        else if (pid == 0)
-        {
-            dup2(in, STDIN_FILENO);
-            if (in != 0)
-                close(in);
-            if (p1->cmds->next != NULL)
-            {
-                dup2(fd[1], STDOUT_FILENO);
-                close(fd[1]);
-            }
-            close(fd[0]);
-            execve(n1->full_path, n1->full_cmd, p1->envp);
-        }
-        else
-        {
-            waitpid(pid, NULL, 0);
-            close(fd[1]);
-            if (in != 0)
-                close(in);
-            in = fd[0];
-        }
-        p1->cmds = p1->cmds->next;
-        if (p1->cmds != NULL)
-            n1 = p1->cmds->content;
-    }
-    close(in);
+	in = 0;
+	p1 = p;
+	n1 = p->cmds->content;
+	while (p1->cmds != NULL)
+	{
+		if (pipe(fd) == -1)
+			ft_exit("pipe error:");
+		pid = fork();
+		if (pid == -1)
+			ft_exit("fork error:");
+		else if (pid == 0)
+		{
+			dup2(in, STDIN_FILENO);
+			if (in != 0)
+				close(in);
+			if (p1->cmds->next != NULL)
+			{
+				dup2(fd[1], STDOUT_FILENO);
+				close(fd[1]);
+			}
+			close(fd[0]);
+			execve(n1->full_path, n1->full_cmd, p1->envp);
+		}
+		else
+		{
+			waitpid(pid, NULL, 0);
+			close(fd[1]);
+			if (in != 0)
+				close(in);
+			in = fd[0];
+		}
+		p1->cmds = p1->cmds->next;
+		if (p1->cmds != NULL)
+			n1 = p1->cmds->content;
+	}
+	close(in);
 }
