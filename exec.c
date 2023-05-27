6194b82415c896bcc61875e83b9a7ff37d8c9016
thed6bel <40074132+thed6bel@users.noreply.github.com>
2023-05-27 20:30:40 +0200

    Update exec.c

--- exec.c
+++ exec.c
@@ -39,19 +39,14 @@ void	ft_free(char **str)
 	free(str);
 }
 
-int ft_execute_builtin(t_mini *n, t_prompt *p)
+int ft_execute_builtin_cd(t_mini *n, t_prompt *p)
 {
-	int		l;
-	t_env	*env_list;
-	pid_t pid;
-	
-	l = ft_strlen(*n->full_cmd);
-		
+	int l = ft_strlen(*n->full_cmd);
 	if (!ft_strncmp(*n->full_cmd, "cd", l) && l == 2)
 	{
-		env_list = get_env_list(p->envp);
+		t_env *env_list = get_env_list(p->envp);
 		ft_builtin_cd(n, env_list);
-		pid = fork();
+		pid_t pid = fork();
 		if (pid == 0)
 			exit(0);
 		else
@@ -60,21 +55,39 @@ int ft_execute_builtin(t_mini *n, t_prompt *p)
 		p->envp = env_list_to_tab(env_list);
 		return (1);
 	}
-	else if (!ft_strncmp(*n->full_cmd, "echo", l) && l == 4)
+	return (0);
+}
+
+int ft_execute_builtin_echo(t_mini *n)
+{
+	int l = ft_strlen(*n->full_cmd);
+	if (!ft_strncmp(*n->full_cmd, "echo", l) && l == 4)
 	{
 		ft_buildin_echo(n);
 		return (1);
 	}
-	else if (!ft_strncmp(*n->full_cmd, "pwd", l) && l == 3)
+	return (0);
+}
+
+int ft_execute_builtin_pwd(t_mini *n)
+{
+	int l = ft_strlen(*n->full_cmd);
+	if (!ft_strncmp(*n->full_cmd, "pwd", l) && l == 3)
 	{
 		ft_builtin_pwd(n);
 		return (1);
 	}
-	else if (!ft_strncmp(*n->full_cmd, "env", l) && l == 3)
+	return (0);
+}
+
+int ft_execute_builtin_env(t_mini *n, t_prompt *p)
+{
+	int l = ft_strlen(*n->full_cmd);
+	if (!ft_strncmp(*n->full_cmd, "env", l) && l == 3)
 	{
-		env_list = get_env_list(p->envp);
+		t_env *env_list = get_env_list(p->envp);
 		built_env(n, env_list);
-		pid = fork();
+		pid_t pid = fork();
 		if (pid == 0)
 			exit(0);
 		else
@@ -83,11 +96,17 @@ int ft_execute_builtin(t_mini *n, t_prompt *p)
 		p->envp = env_list_to_tab(env_list);
 		return (1);
 	}
-	else if (!ft_strncmp(*n->full_cmd, "export", l) && l == 6)
+	return (0);
+}
+
+int ft_execute_builtin_export(t_mini *n, t_prompt *p)
+{
+	int l = ft_strlen(*n->full_cmd);
+	if (!ft_strncmp(*n->full_cmd, "export", l) && l == 6)
 	{
-		env_list = get_env_list(p->envp);
+		t_env *env_list = get_env_list(p->envp);
 		ft_builtin_export(n, env_list);
-		pid = fork();
+		pid_t pid = fork();
 		if (pid == 0)
 			exit(0);
 		else
@@ -96,11 +115,17 @@ int ft_execute_builtin(t_mini *n, t_prompt *p)
 		p->envp = env_list_to_tab(env_list);
 		return (1);
 	}
-	else if (!ft_strncmp(*n->full_cmd, "unset", l) && l == 5)
+	return (0);
+}
+
+int ft_execute_builtin_unset(t_mini *n, t_prompt *p)
+{
+	int l = ft_strlen(*n->full_cmd);
+	if (!ft_strncmp(*n->full_cmd, "unset", l) && l == 5)
 	{
-		env_list = get_env_list(p->envp);
+		t_env *env_list = get_env_list(p->envp);
 		ft_builtin_unset(n, &env_list);
-		pid = fork();
+		pid_t pid = fork();
 		if (pid == 0)
 			exit(0);
 		else
@@ -109,79 +134,136 @@ int ft_execute_builtin(t_mini *n, t_prompt *p)
 		p->envp = env_list_to_tab(env_list);
 		return (1);
 	}
-	else if (!ft_strncmp(*n->full_cmd, "exit", l) && l == 4)
+	return (0);
+}
+
+int ft_execute_builtin_exit(t_mini *n)
+{
+	int l = ft_strlen(*n->full_cmd);
+	if (!ft_strncmp(*n->full_cmd, "exit", l) && l == 4)
 	{
 		ft_builtin_exit(n);
 		return (1);
 	}
+	return (0);
+}
+
+int ft_execute_builtin(t_mini *n, t_prompt *p, int i)
+{
+	int		l;
+	t_env	*env_list;
+	pid_t	pid;
+
+	if (i == 1)
+		return (1);
+	l = ft_strlen(*n->full_cmd);
+	if (ft_execute_builtin_cd(n, p))
+		return (1);
+	else if (ft_execute_builtin_echo(n))
+		return (1);
+	else if (ft_execute_builtin_pwd(n))
+		return (1);
+	else if (ft_execute_builtin_env(n, p))
+		return (1);
+	else if (ft_execute_builtin_export(n, p))
+		return (1);
+	else if (ft_execute_builtin_unset(n, p))
+		return (1);
+	else if (ft_execute_builtin_exit(n))
+		return (1);
 	else
 		return (0);
 }
 
-void ft_execute_single_command(t_mini *cmd, char **envp)
+void ft_configure_io(t_mini *cmd)
 {
-	int status;
-	pid_t pid;
-	
-	pid = fork();
-	if (pid == 0)
-	{ // Sous-processus
-		if (cmd->infile != 0)
-		{
-			dup2(cmd->infile, 0);
-			close(cmd->infile);
-		}
-		if (cmd->outfile != 1)
-		{
-			dup2(cmd->outfile, 1);
-			close(cmd->outfile);
-		}
-		if (cmd->full_path != NULL)
-		{
-			execve(cmd->full_path, cmd->full_cmd, envp);
-			ft_exit("execve ");
-		}
+	if (cmd->infile != 0)
+	{
+		dup2(cmd->infile, 0);
+		close(cmd->infile);
 	}
-	else
-	{ // Processus parent
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status) != 0)
-			g_status = WEXITSTATUS(status);
+	if (cmd->outfile != 1)
+	{
+		dup2(cmd->outfile, 1);
+		close(cmd->outfile);
 	}
 }
 
-void ft_execute_piped_commands(t_list *cmds, t_prompt *p)
+void ft_execute_command(t_mini *cmd, char **envp)
 {
-	t_list *cur;
-	t_mini *cmd;
-	int pipe_fds[2];
-	int saved_stdin;
-	int saved_stdout;
+	if (cmd->full_path != NULL)
+	{
+		execve(cmd->full_path, cmd->full_cmd, envp); 
+		ft_exit("execve ");
+	}
+}
 
+void ft_wait_for_child(pid_t pid, t_mini *cmd)
+{
+	int status;
+	waitpid(pid, &status, 0);
+	//if (WIFEXITED(status) != 0)
+	//g_status = WEXITSTATUS(status);
+}
+
+void ft_close_io(t_mini *cmd)
+{
+	if (cmd->outfile)
+		close(cmd->outfile);
+	if (cmd->infile)
+		close(cmd->infile);
+}
+
+void ft_execute_single_command(t_mini *cmd, char **envp)
+{
+	pid_t pid = fork();
+	if (pid == 0)
+	{ // Sous-processus
+		ft_configure_io(cmd);
+		ft_execute_command(cmd, envp);
+	}
+	else
+	{ // Processus parent
+		ft_wait_for_child(pid, cmd);
+		ft_close_io(cmd);
+	}
+}
+
+void    ft_execute_piped_commands(t_list *cmds, t_prompt *p)
+{
+	t_list	*cur;
+	t_mini	*cmd;
+	int		pipe_fds[2];
+	int		saved_stdin;
+	int		saved_stdout;
+	
 	cur = cmds;
 	saved_stdin = dup(0);
 	saved_stdout = dup(1);
 	while (cur)
 	{
 		cmd = cur->content;
-		if (cur->next)
+		if (cur->next) 
 		{
 			pipe(pipe_fds);
-			cmd->outfile = pipe_fds[1];
+			if (cmd->outfile == 1) 
+				cmd->outfile = pipe_fds[1];  
 			((t_mini *)cur->next->content)->infile = pipe_fds[0];
 		}
-		if (ft_execute_builtin(cmd, p)) 
-		{
-			pid_t pid = fork();
-			if (pid == 0)
-				exit(0);
-			else
-				waitpid(pid, NULL, 0);
+		if (cmd->outfile != 1) 
+		{  
+			if (!ft_execute_builtin(cmd, p, 1))// 1 = on execute pas les builtins  
+			{  
+				dup2(cmd->outfile, 1);  
+				close(cmd->outfile);
+			}
 		}
-		else
+		if (!ft_execute_builtin(cmd, p, 0)) // 0 = on execute les builtins
 			ft_execute_single_command(cmd, p->envp);
 		if (cur->next)
 			close(pipe_fds[1]);
+		if (cmd->outfile != 1)  
+			dup2(saved_stdout, 1);
 		cur = cur->next;
 	}
 	close(pipe_fds[0]);
