59f11fe92038de2081162c31a2dfadb3b56f2458
thed6bel <40074132+thed6bel@users.noreply.github.com>
2023-05-29 18:38:01 +0200

    Update exec.c

--- exec.c
+++ exec.c
@@ -39,12 +39,20 @@ void	ft_free(char **str)
 	free(str);
 }
 
+void ft_close_fds(t_mini *cmd)
+{
+    if (cmd->infile != 0)
+        close(cmd->infile);
+    if (cmd->outfile != 1)
+        close(cmd->outfile);
+}
+
 int ft_execute_builtin(t_mini *n, t_prompt *p, int i)
 {
     t_env *env_list;
 
     if (i == 1)
-        return (1);
+        return 1;
     env_list = get_env_list(p->envp);
     if (!ft_strncmp(*n->full_cmd, "cd", ft_strlen(*n->full_cmd)))
         ft_builtin_cd(n, env_list);
@@ -59,22 +67,28 @@ int ft_execute_builtin(t_mini *n, t_prompt *p, int i)
     else if (!ft_strncmp(*n->full_cmd, "unset", ft_strlen(*n->full_cmd)))
         ft_builtin_unset(n, &env_list);
     else if (!ft_strncmp(*n->full_cmd, "exit", ft_strlen(*n->full_cmd)))
-        ft_builtin_exit(n);
+        ft_builtin_exit(n, env_list);
     else
-        return (0);
+    {
+        free_env_list(env_list);
+        return 0;
+    }
+	ft_close_fds(n);
     ft_free(p->envp);
     p->envp = env_list_to_tab(env_list);
-    return (1);
+    free_env_list(env_list);
+    return 1;
 }
 
 void ft_execute_single_command(t_mini *cmd, char **envp)
 {
-	int	status;
-	pid_t	pid;
+	int status;
+	pid_t pid;
 	
 	pid = fork();
 	if (pid == 0) 
-	{ 
+	{
+		ft_signals_inprocess();
 		if (cmd->infile != 0)
 		{
 			dup2(cmd->infile, 0);
@@ -101,10 +115,10 @@ void ft_execute_single_command(t_mini *cmd, char **envp)
 	}
 }
 
-void    ft_execute_piped_commands(t_list *cmds, t_prompt *p)
+void ft_execute_piped_commands(t_list *cmds, t_prompt *p)
 {
-	t_list		*cur;
-	t_mini		*cmd;
+	t_list	*cur;
+	t_mini	*cmd;
 	int		pipe_fds[2];
 	int		saved_stdin;
 	int		saved_stdout;
@@ -112,6 +126,8 @@ void    ft_execute_piped_commands(t_list *cmds, t_prompt *p)
 	cur = cmds;
 	saved_stdin = dup(0);
 	saved_stdout = dup(1);
+	pipe_fds[0] = -1;
+	pipe_fds[1] = -1;
 	while (cur)
 	{
 		cmd = cur->content;
@@ -122,23 +138,16 @@ void    ft_execute_piped_commands(t_list *cmds, t_prompt *p)
 				cmd->outfile = pipe_fds[1];
 			((t_mini *)cur->next->content)->infile = pipe_fds[0];
 		}
-		if (cmd->outfile != 1) 
-		{  
-			if (!ft_execute_builtin(cmd, p, 1))// 1 = on execute pas les builtins  
-			{  
-				dup2(cmd->outfile, 1);  
-				close(cmd->outfile);
-			}
-		}
-		if (!ft_execute_builtin(cmd, p, 0)) // 0 = on execute les builtins
+		if (!ft_execute_builtin(cmd, p, 0))
 			ft_execute_single_command(cmd, p->envp);
 		if (cur->next)
 			close(pipe_fds[1]);
-		if (cmd->outfile != 1)  
+		if (cmd->outfile != 1)
 			dup2(saved_stdout, 1);
 		cur = cur->next;
 	}
-	close(pipe_fds[0]);
+	if (pipe_fds[0] != -1)
+		close(pipe_fds[0]);
 	dup2(saved_stdin, 0);
 	close(saved_stdin);
 	dup2(saved_stdout, 1);
