d861f080a70b9c6d027ae84047d87a8d86684021
thed6bel <40074132+thed6bel@users.noreply.github.com>
2023-05-29 18:41:51 +0200

    Update ft_substr.c

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lowathar <lowathar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: thed6bel <thed6bel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 17:27:33 by lwathar           #+#    #+#             */
-/*   Updated: 2022/10/04 14:44:01 by lowathar         ###   ########.fr       */
+/*   Updated: 2023/05/28 23:51:51 by thed6bel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,16 +31,17 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*s1;
 	size_t	i;
 
-	s1 = NULL;
-	i = 0;
-	if (s == 0 || start > ft_strlen(s))
+	if (s == NULL)
+		return (NULL);
+	if (start >= ft_strlen(s))
 		return (ft_protect());
-	if (len > ft_strlen(s))
-		len = ft_strlen(s);
+	if (len > ft_strlen(s) - start)
+		len = ft_strlen(s) - start;
 	s1 = malloc(sizeof(*s) * (len + 1));
 	if (s1 != NULL)
 	{
-		while (i < len && i < ft_strlen(s))
+		i = 0;
+		while (i < len)
 		{
 			s1[i] = s[i + start];
 			i++;
