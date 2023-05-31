e2a0f20f351c758b24f054fa9a1fb432cc6b81b4
thed6bel <40074132+thed6bel@users.noreply.github.com>
2023-05-31 18:34:51 +0200

    split functions

--- exec.c
+++ exec.c
@@ -12,6 +12,31 @@
 
 #include "minishell.h"
 
+void	ft_exec_builtin_helper(t_mini *n, t_env *env_list, t_prompt *p)
+{
+	ft_close_fds(n);
+	ft_free(p->envp);
+	p->envp = env_list_to_tab(env_list);
+	free_env_list(env_list);
+}
+
+int	ft_execute_other_builtins(t_mini *n, t_prompt *p, t_env *env_list)
+{
+	if (!ft_strncmp(*n->full_cmd, "export", ft_strlen(*n->full_cmd) + 1))
+		ft_builtin_export(n, env_list);
+	else if (!ft_strncmp(*n->full_cmd, "unset", ft_strlen(*n->full_cmd) + 1))
+		ft_builtin_unset(n, &env_list);
+	else if (!ft_strncmp(*n->full_cmd, "exit", ft_strlen(*n->full_cmd) + 1))
+		ft_builtin_exit(n, env_list);
+	else
+	{
+		free_env_list(env_list);
+		return (0);
+	}
+	ft_exec_builtin_helper(n, env_list, p);
+	return (1);
+}
+
 int	ft_execute_builtin(t_mini *n, t_prompt *p)
 {
 	t_env	*env_list;
@@ -25,22 +50,39 @@ int	ft_execute_builtin(t_mini *n, t_prompt *p)
 		ft_builtin_pwd(n);
 	else if (!ft_strncmp(*n->full_cmd, "env", ft_strlen(*n->full_cmd) + 1))
 		built_env(n, env_list);
-	else if (!ft_strncmp(*n->full_cmd, "export", ft_strlen(*n->full_cmd) + 1))
-		ft_builtin_export(n, env_list);
-	else if (!ft_strncmp(*n->full_cmd, "unset", ft_strlen(*n->full_cmd) + 1))
-		ft_builtin_unset(n, &env_list);
-	else if (!ft_strncmp(*n->full_cmd, "exit", ft_strlen(*n->full_cmd) + 1))
-		ft_builtin_exit(n, env_list);
 	else
-	{
-		free_env_list(env_list);
-		return (0);
-	}
-	ft_close_fds(n);
-	ft_free(p->envp);
-	p->envp = env_list_to_tab(env_list);
-	free_env_list(env_list);
+		return (ft_execute_other_builtins(n, p, env_list));
+	ft_exec_builtin_helper(n, env_list, p);
 	return (1);
+}
+
+void	ft_child_process(t_mini *cmd, char **envp)
+{
+	if (cmd->infile != 0)
+	{
+		dup2(cmd->infile, 0);
+		close(cmd->infile);
+	}
+	if (cmd->outfile != 1)
+	{
+		dup2(cmd->outfile, 1);
+		close(cmd->outfile);
+	}
+	if (cmd->full_path != NULL)
+	{
+		execve(cmd->full_path, cmd->full_cmd, envp);
+		ft_exit("execve ");
+	}
+	exit(0);
+}
+
+void	ft_parent_process(t_mini *cmd, int *status, pid_t pid)
+{
+	waitpid(pid, status, 0);
+	if (WIFEXITED(*status) != 0)
+		g_status = WEXITSTATUS(*status);
+	close(cmd->outfile);
+	close(cmd->infile);
 }
 
 void	ft_execute_single_command(t_mini *cmd, char **envp)
@@ -51,43 +93,15 @@ void	ft_execute_single_command(t_mini *cmd, char **envp)
 	pid = fork();
 	ft_signals_inprocess();
 	if (pid == 0)
-	{
-		if (cmd->infile != 0)
-		{
-			dup2(cmd->infile, 0);
-			close(cmd->infile);
-		}
-		if (cmd->outfile != 1)
-		{
-			dup2(cmd->outfile, 1);
-			close(cmd->outfile);
-		}
-		if (cmd->full_path != NULL)
-		{
-			execve(cmd->full_path, cmd->full_cmd, envp);
-			ft_exit("execve ");
-		}
-		exit(0);
-	}
+		ft_child_process(cmd, envp);
 	else
-	{
-		waitpid(pid, &status, 0);
-		if (WIFEXITED(status) != 0)
-			g_status = WEXITSTATUS(status);
-		close(cmd->outfile);
-		close(cmd->infile);
-	}
+		ft_parent_process(cmd, &status, pid);
 }
 
-void	ft_execute_p_cmds(t_list *cmds, t_prompt *p, t_list *cur, int fds[2])
+void ft_pipe_and_execute(t_list *cur, t_prompt *p, int fds[2], int saved_stdout)
 {
-	t_mini	*cmd;
-	int		saved_stdin;
-	int		saved_stdout;
+	t_mini *cmd;
 
-	cur = cmds;
-	saved_stdin = dup(0);
-	saved_stdout = dup(1);
 	while (cur)
 	{
 		cmd = cur->content;
@@ -106,6 +120,10 @@ void	ft_execute_p_cmds(t_list *cmds, t_prompt *p, t_list *cur, int fds[2])
 			dup2(saved_stdout, 1);
 		cur = cur->next;
 	}
+}
+
+void ft_close_and_restore(int fds[2], int saved_stdin, int saved_stdout, t_list *cmds)
+{
 	if (fds[0] != -1)
 		close(fds[0]);
 	ft_lstclear(&cmds, free_content);
@@ -113,6 +131,19 @@ void	ft_execute_p_cmds(t_list *cmds, t_prompt *p, t_list *cur, int fds[2])
 	close(saved_stdin);
 	dup2(saved_stdout, 1);
 	close(saved_stdout);
+}
+
+void ft_execute_p_cmds(t_list *cmds, t_prompt *p, t_list *cur, int fds[2])
+{
+	t_mini *cmd;
+	int saved_stdin;
+	int saved_stdout;
+
+	cur = cmds;
+	saved_stdin = dup(0); 
+	saved_stdout = dup(1);  
+	ft_pipe_and_execute(cur, p, fds, saved_stdout);  
+	ft_close_and_restore(fds, saved_stdin, saved_stdout, cmds);
 }
 
 void	ft_execute_commandes(t_prompt *p)
