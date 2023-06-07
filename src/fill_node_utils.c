7a96251422c75bbf4f7a4108ee68fff4045d93ff
Louis Wathar <lowathar@shi-r9-p5.s19.be>
2023-06-07 13:51:50 +0200

    >> ok

--- src/fill_node_utils.c
+++ src/fill_node_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fill_node_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hucorrei <hucorrei@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lowathar <lowathar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 09:43:58 by hucorrei          #+#    #+#             */
-/*   Updated: 2023/06/07 09:55:00 by hucorrei         ###   ########.fr       */
+/*   Updated: 2023/06/07 12:42:12 by lowathar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,15 @@ int	ft_check_error(char **a[2], int *i)
 		*i = -2;
 		return (-1);
 	}
-	else if ((a[0][*i][0] == '>' && a[0][*i + 1]) && (a[0][*i + 1][0] == '>'))
+	else if ((a[0][*i][0] == '>' && a[0][*i + 2]) && (a[0][*i + 2][0] == '>'))
 	{
 		g_status = 258;
 		ft_putstr_fd("minishell: syntax error near unexpected token `>'\n", 2);
 		*i = -2;
 		return (-1);
 	}
-	else if ((a[0][*i][0] == '>' && a[0][*i + 1]) && (a[0][*i + 1][0] == '<'))
+	else if (((a[0][*i][0] == '>') && (((a[0][*i + 1]) && (a[0][*i + 1][0] \
+	== '<')) || ((a[0][*i + 2]) && (a[0][*i + 2][0] == '<')))))
 	{
 		g_status = 258;
 		ft_putstr_fd("minishell: syntax error near unexpected token `<'\n", 2);
